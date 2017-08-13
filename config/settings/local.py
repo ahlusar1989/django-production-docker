48d4373d3e8e96c85e317294c2eb5ff5420726cb
SARAN AHLUWALIA <ahlusar.ahluwalia@gmail.com>
2017-08-13 11:26:50 -0400

    shuffling and adding environment

---
+++ config/settings/local.py
@@ -0,0 +1,7 @@
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'gg0kcl&m1nxt2j_jn=mp_q50gdvn82jr^waqd3_c)nil)a=a#c'
+
+DEBUG = env.bool('DJANGO_DEBUG', default=True)
