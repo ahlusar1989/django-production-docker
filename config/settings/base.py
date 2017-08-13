2c801f4cfbb0968c6d4bdc4932022af2de0996db
SARAN AHLUWALIA <ahlusar.ahluwalia@gmail.com>
2017-08-13 11:46:12 -0400

    added API as app of project - added new apps to the settings config

--- config/settings/base.py
+++ config/settings/base.py
@@ -54,9 +54,11 @@ DJANGO_APPS = (
 )
 
 THIRD_PARTY_APPS = (
+    'rest_framework',
 )
 
 LOCAL_APPS = (
+    'project.api',
 )
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
@@ -152,3 +154,6 @@ STATICFILES_FINDERS = (
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = str(APPS_DIR('media'))
+
+REST_FRAMEWORK = {
+}
