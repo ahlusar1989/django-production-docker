dcbb13718810ca6712590aac55989a682a01232f
SARAN AHLUWALIA <ahlusar.ahluwalia@gmail.com>
2017-08-14 12:24:40 -0400

    add Procfile, for Heroku deployment with gunicorn to manage number of workers

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,7 +1,9 @@
+from __future__ import absolute_import
 from .base import *
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'gg0kcl&m1nxt2j_jn=mp_q50gdvn82jr^waqd3_c)nil)a=a#c'
+SECRET_KEY = env('DJANGO_SECRET_KEY',
+	default='gg0kcl&m1nxt2j_jn=mp_q50gdvn82jr^waqd3_c)nil)a=a#c')
 
 DEBUG = env.bool('DJANGO_DEBUG', default=True)
