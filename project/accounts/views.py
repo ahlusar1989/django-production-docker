976eb73caaa46f44e54e0c55a7fbd297b2acb37e
SARAN AHLUWALIA <ahlusar.ahluwalia@gmail.com>
2017-08-14 14:21:08 -0400

    added travis build

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -21,11 +21,14 @@ class UserRegisterView(AtomicMixin, GenericAPIView):
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
+
         self.perform_create(serializer)
+        
         headers = self.get_success_headers(serializer.data)
         username = serializer.data["username"]
         user = get_object_or_404(User, username=username)
         token = AuthToken.objects.create(user)
+
         return Response({
             'user': UserSerializer(user).data,
             'token': token
