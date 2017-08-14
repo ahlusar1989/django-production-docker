9fe28605dbfd2c6d79b958d05c7b1be2670747a1
SARAN AHLUWALIA <ahlusar.ahluwalia@gmail.com>
2017-08-14 11:05:06 -0400

    change app to users

--- project/users/views.py
+++ project/users/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
-from project.api.serializers import UserSerializer
+from project.users.serializers import UserSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     """
